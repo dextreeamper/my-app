ea6fb6ddd4f2293cacb59ff1e7efe0868eba261e
Dextree Amper <appsdex92@gmail.com>
2017-10-23 23:13:11 +0800

    Installing react-bootstrap and import the component to resource file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+// This will be the src index of react project
 import React, { Component } from 'react';
 import logo from './assets/images/logo.svg';
-import './assets/css/App.css'
+import './assets/css/App.css';
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+         <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">React-Bootstrap</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link One</NavItem>
+            <NavItem eventKey={2} href="#">Link Two</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hello to React</h1>
